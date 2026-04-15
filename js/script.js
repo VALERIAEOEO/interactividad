e456e5b58ce2a2b7180abe652bf15a96196c606b
Valeria <vestrada@centro.edu.mx>
2026-04-14 19:56:43 -0600

    added_resta

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 let a =1;
 let b = 2;
 
-function suma () {
+function suma (a=0, b=0){ {
     const resultado = a + b;
     alert(resultado);
 }
 
-suma();
+function resta (a=0, b=0){ {
+    const resultado = a - b;
+    alert(resultado);
+}
+
+suma(2, 5);
+resta(5, 2);
