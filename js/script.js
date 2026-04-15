7624868b16f7e545790668a31d8b3641a93d5618
Valeria <vestrada@centro.edu.mx>
2026-04-14 20:07:57 -0600

    added_bg_color_in_script

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
-let a =1;
+/* pop-ups matematicos
+
+let a = 1;
 let b = 2;
 
-function suma (a=0, b=0){ {
+function suma(a = 0, b = 0) {
+    
+        const resultado = a + b;
+        alert(resultado);
+    }
+
+    function resta(a = 0, b = 0) {
+        
+            const resultado = a - b;
+            alert(resultado);
+        }
+
+        suma(2, 5);
+        resta(5, 2);
+        */
+
+function suma(a = 0, b = 0) {
     const resultado = a + b;
-    alert(resultado);
+    console.log(resultado);
 }
-
-function resta (a=0, b=0){ {
-    const resultado = a - b;
-    alert(resultado);
-}
-
-suma(2, 5);
-resta(5, 2);
+document.addEventListener("click", function() {
+    document.body.style.backgroundColor = "lightblue";
+});
