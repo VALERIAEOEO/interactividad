5db8193cfc19a5f9082d317f9285ef6be137c17c
Valeria <vestrada@centro.edu.mx>
2026-04-16 18:19:23 -0600

    made_input_and_bof

---
+++ js/input.js
@@ -0,0 +1,12 @@
+let title = document.querySelector("h1");
+let inputText = document.getElementById("input-text");
+let inputcolor = document.getElementById("input-color");
+inputText.addEventListener("input", function() {
+    const value = inputText.value;
+    title.innerText = value;
+});
+
+inputcolor.addEventListener("input", function() {
+    const color = inputcolor.value;
+    title.style.color = color;
+});
