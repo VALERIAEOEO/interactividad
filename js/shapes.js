209a05901cc8b3c203b0cf51a32754c398da3c59
Valeria <vestrada@centro.edu.mx>
2026-04-23 15:41:02 -0600

    changes_js

--- js/shapes.js
+++ js/shapes.js
@@ -4,4 +4,14 @@ function setup() {
 
 function draw() {
         background(0);
+        stroke("white");
+        noFill()
+        rect(x, y, 100, 100);
+
+        x += 1;
+        y += 1;
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
 }
