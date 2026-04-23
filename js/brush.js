31be5aded04591cf0da22a65123109b678c374a0
Valeria <vestrada@centro.edu.mx>
2026-04-23 16:51:43 -0600

    can_draw_on_brush

--- js/brush.js
+++ js/brush.js
@@ -1,10 +1,34 @@
+const PALETTE = ["white", "black", "blue", "magenta", "purple"];
+let bg_color =0;
+let stroke_color = 0;
+let stroke_weight = 0;
+let cords =[];
+
 function setup () {
     createCanvas(windowWidth, windowHeight);
+    bg_color = random(PALETTE);
+    stroke_color = random(PALETTE);
+    stroke_weight = random(42, 100);
 }
 
 function draw() {
-    background(random(0,100), random(0,100), random(130,200), 20);
-    fill(random(0,255), random(0,255), random(0,255), 50);
-    stroke("goldenrod");
-    ellipse(mouseX, mouseY, 25, 25);
+       background(bg_color);
+       stroke(stroke_color);
+       strokeWeight(stroke_weight);
+       noFill();
+
+        beginShape();
+        for (let i = 0; i < cords.length; i++) {
+            const [x,y] = cords[i];
+            vertex(x,y);
+        }
+        endShape();
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function mouseDragged() {
+    cords.push([mouseX, mouseY]);   
 }
